03fa9be326717e817b74720be84db48ae3aa825c
adettelle <l.litvinski@gmail.com>
2026-06-07 19:31:12 +0200

    refactor solution

--- sprint_1/1_nearest_zero/main.go
+++ sprint_1/1_nearest_zero/main.go
@@ -1,9 +1,10 @@
-// https://contest.yandex.ru/contest/22450/run-report/162781650/
+// https://contest.yandex.ru/contest/22450/run-report/162860542/
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -16,9 +17,7 @@ func main() {
 	n := readInt(scanner)
 	arr := readArray(scanner)
 
-	zeros := Zeros(arr)
-
-	res := getNearestZeroDistances(zeros, n)
+	res := getNearestZeroDistances(arr, n)
 
 	for _, num := range res {
 		b.WriteString(strconv.Itoa(num))
@@ -28,51 +27,40 @@ func main() {
 	fmt.Print(b.String())
 }
 
-func getNearestZeroDistances(zeros []int, n int) []int {
-	res := make([]int, n)
+func getNearestZeroDistances(arr []int, n int) []int {
+	maxInt := math.MaxInt32
+	counter := 1
+	afterZero := false
 
-	// before zeros[0]
-	for i := zeros[0]; i >= 0; i-- {
-		res[i] = zeros[0] - i
-	}
-
-	// after zeros[0] and before zeros[last]
-	for k := 0; k < len(zeros)-1; k++ {
-		sl := res[zeros[k] : zeros[k+1]+1]
-
-		middle := len(sl) / 2
-		if len(sl)%2 != 0 {
-			middle++
-		}
-		for j := 0; j < middle; j++ {
-			sl[j] = j
-			sl[len(sl)-j-1] = j
-		}
-	}
-
-	// after zeros[last]
-	num := 0
-	for i := zeros[len(zeros)-1]; i < n; i++ {
-		res[i] = num
-		num++
-	}
-	return res
-}
-
-func Zeros(arr []int) []int {
-	zeros := []int{}
-
-	if len(arr) == 1 {
-		arr[0] = 0
-		return arr
-	}
-
-	for i := 0; i < len(arr); i++ {
+	for i := 0; i < n; i++ {
 		if arr[i] == 0 {
-			zeros = append(zeros, i)
+			afterZero = true
+			counter = 1
+			continue
+		}
+		if !afterZero {
+			arr[i] = maxInt
+		} else {
+			arr[i] = counter
+			counter++
 		}
 	}
-	return zeros
+
+	afterZero = false
+	for i := n - 1; i >= 0; i-- {
+		if arr[i] == 0 {
+			afterZero = true
+			counter = 1
+			continue
+		}
+		if afterZero {
+			if counter < arr[i] {
+				arr[i] = counter
+			}
+			counter++
+		}
+	}
+	return arr
 }
 
 func makeScanner() *bufio.Scanner {
